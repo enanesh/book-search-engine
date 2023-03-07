ae8502740fa52c612531a143e385bbd7395562f2
enanesh <edna.ganesha.gr@gmail.com>
2023-03-07 13:23:41 -0800

    Adds the schema folder, and imports ApolloServer on 'server.js' also creates the new instance in ApolloServer with graphQl schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+//Imports the ApolloServer Class 
+const { ApolloServer } = require("@apollo/server");
+
+
+const { expressMiddleware } = require("@apollo/server/express4");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+const http = require("http");
+
+
+
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const httpServer = http.createServer(app);
+
+
+
+
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
