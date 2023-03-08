673eba5b97716e571e59b14661160a8e57756c9c
enanesh <edna.ganesha.gr@gmail.com>
2023-03-08 01:05:21 -0800

    Sets the server, still having a midleware bug

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+
 
 // Import the ApolloServer class
 const { ApolloServer } = require("@apollo/server");
-
 const { expressMiddleware } = require("@apollo/server/express4");
-
-const {
-  ApolloServerPluginDrainHttpServer,
-} = require("@apollo/server/plugin/drainHttpServer");
-const http = require("http");
-
+const {ApolloServerPluginDrainHttpServer} = require("@apollo/server/plugin/drainHttpServer");
 const { typeDefs, resolvers } = require("./schemas");
 
+const http = require("http");
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,33 +17,24 @@ const app = express();
 
 const httpServer = http.createServer(app);
 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-app.use(express.urlencoded({ extended: false }));
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-// app.use(routes);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-
-
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -57,14 +43,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   app.use(
     "/graphql",
     expressMiddleware(server, {
-      context: authMiddleware,
+      context: async({req}) => ({token:req.headers.token}),
     })
   );
 
   db.once("open", () => {
     httpServer.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at localhost:${PORT}/graphql`);
     });
   });
 };
