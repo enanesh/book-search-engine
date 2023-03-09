af11ba0d19ae38c5467aed33bf2788d12372afc6
enanesh <edna.ganesha.gr@gmail.com>
2023-03-08 23:01:37 -0800

    Sets the typeDef and resolvers, It's working ut still havin issues with a dreprecated version of react-dom

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,86 @@
+const { GraphQLError } = require("graphql");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                return userData;
+            }
+            throw new GraphQLError("You need to be logged in!", {
+                extensions: {
+                    code: "UNAUTHENTICATED",
+                },
+            });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new GraphQLError("No profile with this email found!", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                    },
+                });
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new GraphQLError("Incorrect password!", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                    },
+                });
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    {
+                        $addToSet:
+                            { savedBooks: book }
+                    },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new GraphQLError("You need to be logged in!", {
+                extensions: {
+                    code: "UNAUTHENTICATED",
+                },
+            });
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    {
+                        $pull:
+                        {
+                            savedBooks:
+                                { bookId: bookId }
+                        }
+                    },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        }
+    }
+};
 
 
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
+const typeDefs = `#graphql
+ 
+    type user{
+     _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [String]
+    },
 
+    type Book{
+    _id: ID
+    authors: [String]
+    bookId: String
+    forSale: String
+    link: String
+    title: String
+    },
 
+    type Auth{
+        token: ID!
+        user: User
+    },
 
+    type Query {
+        me: User
+    }
+
+    type Mutation{
+        login(email:String!,password:String!):Auth
+        addUser(username:String!, password:String!):Auth
+        saveBook(input:savedBook!):User
+        removeBook(bookId:ID!):User
+    },
+
+`;
 
 module.exports = typeDefs;
