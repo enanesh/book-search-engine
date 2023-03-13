952cd41e3f47a7313d15431305fc4d6aa319346d
enanesh <edna.ganesha.gr@gmail.com>
2023-03-12 22:30:13 -0700

    Changes added to server for deployment

--- server/server.js
+++ server/server.js
@@ -32,9 +32,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-});
+
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
